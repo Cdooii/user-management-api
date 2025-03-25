415f06cc375915f1134c96dc58b3a805b16d1d20
unknown <barruelrachel06@gmail.com>
2025-03-25 19:42:38 +0800

    Updated

--- user.create.js
+++ user.create.js
@@ -39,5 +39,5 @@ app.post('/register', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
